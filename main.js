acaba271c1a5842fb2a1b97cde0a1ad31b5d8ee8
flekpy <flekpy@gmail.com>
2023-11-08 14:06:30 +0800

    add cube and rectangle

---
+++ main.js
@@ -0,0 +1,19 @@
+function main() {
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext('2d')
+
+  const clientWidth = canvas.clientWidth
+  const clientHeight = canvas.clientHeight
+
+  const imageData = ctx.createImageData(clientWidth, clientHeight)
+
+  const drawer = new Drawer(imageData.data, imageData.width, imageData.height)
+
+  resize(canvas)
+  axisDirection(clientWidth, clientHeight, drawer)
+  drawer.drawCube()
+  drawer.drawRectangle()
+  ctx.putImageData(imageData, 0, 0)
+}
+
+window.addEventListener('resize', () => main())
